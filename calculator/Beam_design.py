a21bfacacb6094bb5279a8186be949123618b478
InquisitiveDesign <106902371+InquisitiveDesign@users.noreply.github.com>
2022-08-29 21:38:35 +0530

    Create Beam_design.py

---
+++ calculator/Beam_design.py
@@ -0,0 +1,27 @@
+def beamd(BL,NS,NL):
+    import math
+    beam_length = float(BL)
+    Num_support = int(NS)
+    Num_load = int(NL)
+
+    Support_type = []
+    loc_supp = []
+    Supp_direc = []
+    for n in range(Num_support):
+        Support_type.append()
+        loc_supp.append()                                            # from left end of the beam in mm
+        Supp_direc.append()
+
+
+    Load_type = []
+    loc_load = []
+    load_direc = []
+    load_val = []
+    for i in range(Num_load):
+        Load_type.append()
+        loc_load.append()
+        load_direc.append()
+        load_val.append()
+
+    #Now for a simply supported beam
+    Totload = sum(load_val)
