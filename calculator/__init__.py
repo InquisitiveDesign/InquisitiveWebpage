d351f38f9d2bfcbda75a026448179f81a2ef746c
InquisitiveDesign <106902371+InquisitiveDesign@users.noreply.github.com>
2023-01-21 23:45:59 +0530

    Update __init__.py

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '471964c08066039057be602f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///design.db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from calculator import bearingroute
