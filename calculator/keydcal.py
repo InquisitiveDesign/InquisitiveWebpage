150deaa46eac76a2b794b96bd2167ecdb01836e5
InquisitiveDesign <106902371+InquisitiveDesign@users.noreply.github.com>
2023-02-17 21:15:32 +0530

    Update keydcal.py

--- calculator/keydcal.py
+++ calculator/keydcal.py
@@ -23,17 +23,22 @@ def key_d(P,N,S,F,D,Th,Kt):
     max_perss = float(sh_st/fos)   #max permissible shear stress
     omega = (2*(math.pi)*Nspeed/60)   #angular speed in rad/sec
     Mt = (power_in*(10**6)/(omega))   #Torque to be transmitted by the shaft to hub via key in N-mm
-    B = round(float(shaftDia/4),2)          #Width and height (in mm) as per the standard industrial practice i.e 1/4th of shaft dia for square key
+
+    B = round(float(shaftDia/4),3)          #Width and height (in mm) as per the standard industrial practice i.e 1/4th of shaft dia for square key
+    
     if key_type == "Sq":
-        H = round(float(shaftDia/4),2)
+        H = round(float(shaftDia/4),3)
+
     elif key_type == "Fl":
-        H = round(float(B*(2/3)),2)
+        H = round(float(B*(2/3)),3)
+
     keylen_ssbased = float(2*Mt/(max_perss*shaftDia*B))
     keylen_csbased = float(4*Mt/(max_percs*shaftDia*H))
+    
     if keylen_ssbased >= keylen_csbased:
-        L = round(keylen_ssbased,2)
+        L = round(keylen_ssbased,3)
 
     elif keylen_ssbased < keylen_csbased:
-        L = round(keylen_csbased,2)
+        L = round(keylen_csbased,3)
 
     return [L,B,H]
